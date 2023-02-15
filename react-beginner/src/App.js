0c09f031f2ed1b06775a82bbb59a08114a542812
joon shim <jshim4996@gmail.com>
2023-02-16 08:42:04 +0900

    23.02.16 useEffect 함수 공부

--- react-beginner/src/App.js
+++ react-beginner/src/App.js
@@ -1,11 +1,34 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [couter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run olny once");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' change ");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' change ");
+  }, [couter]);
+  useEffect(() => {
+    console.log("Irun when 'keyword' && 'counter' change");
+  }, [keyword, couter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
+      <h1 className={styles.title}>{couter}</h1>
+      <button onClick={onClick}>증가</button>
     </div>
   );
 }
