27dd9e15e90471889c15e325be899c8096faf2c5
joon shim <jshim4996@gmail.com>
2023-02-16 09:16:07 +0900

    23.02.16 useEffect destory 기능 학습

--- react-beginner/src/App.js
+++ react-beginner/src/App.js
@@ -1,11 +1,26 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("by");
+  }, []);
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("by");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
